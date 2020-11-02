06bdfc1f77382902e7c76aa6f0d5564d33e80e7f
jimmy-nguyen-cal <nhanjim@berkeley.edu>
2020-11-01 21:16:07 -0800

    "first two chpters"

---
+++ 02_Variables, Expressions, and Statements.py
@@ -0,0 +1,15 @@
+#--- Variables
+x = 12.2
+y = 14
+
+#--- Choosing variable names
+hours = 35.0
+rate = 12.50
+pay = hours * rate
+print(pay)
+
+#--- Assignment Statements
+x = 0.63
+print(x)
+x = 3.9 * x * (1 - x)
+print(x)
