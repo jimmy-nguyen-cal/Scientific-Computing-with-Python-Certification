29cc0bfffd1ad6bdc30d30f89a392e186f9a8674
jimmy-nguyen-cal <nhanjim@berkeley.edu>
2020-11-01 21:24:57 -0800

    "first three chapters"

---
+++ 03_Intermediate Expressions.py
@@ -0,0 +1,19 @@
+
+# --- Order of Evaluation
+# 1. Parenthesis
+# 2. Power
+# 3. Multiplication
+# 4. Addition
+# 5. Subtraction
+
+x = 1 + 2 * 3 - 4 / 5 ** 6
+print(x)
+
+#--- User Input
+nam = input('Who are you? ')
+print('Welcome', nam)
+
+#--- Converting User Input
+inp = input('Europe Floor?')
+usf = int(inp) + 1
+print('US floor', usf)
