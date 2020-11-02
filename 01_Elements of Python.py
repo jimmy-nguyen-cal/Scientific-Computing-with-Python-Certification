e995eeeb64f146dc8cab121d6df13c503da0bb06
jimmy-nguyen-cal <nhanjim@berkeley.edu>
2020-11-01 21:03:04 -0800

    "first two chpters"

---
+++ 01_Elements of Python.py
@@ -0,0 +1,21 @@
+# --- Sequential Steps
+x = 2
+print(x)
+x = x + 2
+print(x)
+
+#--- Conditional Steps
+
+x = 5
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+
+
+#--- Repeated Steps
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print('Blastoff!')
