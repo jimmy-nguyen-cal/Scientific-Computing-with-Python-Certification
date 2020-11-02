5f2d912baa6ef6c7e648346e70b3fb536c147d90
jimmy-nguyen-cal <nhanjim@berkeley.edu>
2020-11-01 22:47:30 -0800

    "first 10 chapters"

--- 09_Definite Loops.py
+++ 09_Definite Loops.py
@@ -1,3 +1,15 @@
 # --- A simple definite loop
 for i in [2,1,5]:
     print(i)
+
+# --- A definite loop with strings
+
+friends = ['Joseph', ' Glenn', 'Sally']
+for friend in friends:
+    print('Happy New Year:', friend)
+print('Done!')
+
+#--- Sumamry
+# the iteration variable "iterates" through the sequence (ordered set)
+# the block(body) of code is executed once for each value in the sequence
+# the iteration variable moves through all of the values in the sequence
