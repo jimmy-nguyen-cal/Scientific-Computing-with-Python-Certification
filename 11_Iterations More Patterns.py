74ac58f6b319bbb1ad21fd6e96a0629d6e8719fa
jimmy-nguyen-cal <nhanjim@berkeley.edu>
2020-11-04 10:15:57 -0800

    more loops

--- 11_Iterations More Patterns.py
+++ 11_Iterations More Patterns.py
@@ -2,38 +2,35 @@
 print('\nCounting ...')
 zork = 0
 print('Before', zork)
-for thing in [9,41,12,3,74.15]:
+for thing in [9, 41, 12, 3, 74.15]:
     zork += 1
     print(zork, thing)
 print('After', zork)
-
 
 # --- Summing in a Loop
 print('\nSumming...')
 zork = 0
 print('Before', zork)
-for thing in [9,41,12,3,74,15]:
+for thing in [9, 41, 12, 3, 74, 15]:
     zork += thing
     print(zork, thing)
 print('After', zork)
-
 
 # --- Finding the average in a loop
 print('\nAveraging ...')
 count = 0
 sum = 0
 print('Before', count, sum)
-for value in [9,41,12,3,74,15]:
+for value in [9, 41, 12, 3, 74, 15]:
     count += 1
     sum += value
     print(count, sum, value)
-print('After', count, sum, sum/count)
-
+print('After', count, sum, sum / count)
 
 # --- Filtering in a loop
 print('\nFiltering ...')
 print('Before')
-for value in [9,41,12,3,74.15]:
+for value in [9, 41, 12, 3, 74.15]:
     if value > 20:
         print('Large number', value)
 print('After')
@@ -42,7 +39,7 @@ print('After')
 print('\nSearching ...')
 found = False
 print('Before', found)
-for value in [9,41,12,3,74,15]:
+for value in [9, 41, 12, 3, 74, 15]:
     if value == 3:
         found = True
     else:
@@ -54,18 +51,18 @@ print('After', found)
 print('\nSearching for the smallest value ...')
 smallest_so_far = -1
 print('Before', smallest_so_far)
-for num in [9,41,12,3,74,15]:
+for num in [9, 41, 12, 3, 74, 15]:
     if num < smallest_so_far:
         smallest_so_far = num
     print(smallest_so_far, num)
 print('After', smallest_so_far)
 
-#--- How he would find the smallest number
-#--- using the IS operator implies 'is the same as' and using None keyword
+# --- How he would find the smallest number
+# --- using the IS operator implies 'is the same as' and using None keyword
 print('\nSearching for the smallest value ...')
 smallest = None
 print('Before', smallest)
-for num in [9,41,12,3,74,15]:
+for num in [9, 41, 12, 3, 74, 15]:
     if smallest is None:
         smallest = num
     elif num < smallest:
